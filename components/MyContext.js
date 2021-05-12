dcea13417b0eb26f1f8d1113e781e78c79064825
Gregory Cowley <gregory@cowleystudios.com>
2021-05-12 05:46:15 -0700

    Changes to configuration and deleted files

--- components/MyContext.js
+++
@@ -1,6 +0,0 @@
-import React, { createContext, useState } from 'react';
-
-import projects from '../data/all-content.json';
-const MyContext = React.createContext(projects);
-
-export default MyContext;
--- next.config.js
+++ next.config.js
@@ -1,29 +1,98 @@
 const withCSS = require("@zeit/next-css");
-const glob = require('glob');
+const glob = require("glob");
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const ASSET_PATH = process.env.ASSET_PATH || "/";
 
 /**
  * next.config.js
  */
 
 module.exports = withCSS({
+  basePath: "",
   cssModules: false,
-  webpack: config => {
+  future: {
+    webpack5: true,
+  },
+  env: {
+    ASSET_PATH: ASSET_PATH,
+  },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
-      use: "raw-loader"
+      use: "raw-loader",
     });
+    config.module.rules.push({
+      test: /\.json5$/i,
+      loader: "json5-loader",
+      type: "javascript/auto",
+    });
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ["babel-loader"],
+    });
+
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    });
+
+    config.module.rules.push({
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: "asset",
+    });
+
+    config.resolve.extensions.concat(["*", ".js", ".jsx"]);
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      assets: path.resolve(__dirname, "src/assets/"),
+      common: path.resolve(__dirname, "src/common/"),
+      hooks: path.resolve(__dirname, "src/hooks/"),
+      components: path.resolve(__dirname, "src/components_new/"),
+      pages: path.resolve(__dirname, "src/pages_new/"),
+    };
+
+    // config.plugins.push(new HotModuleReplacementPlugin());
+
+    // config.plugins.push(
+    //   new HtmlWebpackPlugin({
+    //     title: "Gregory Cowley",
+    //     template: "./src/index.ejs",
+    //     inject: "body",
+    //     filename: "index.html",
+    //   })
+    // );
+
+    config.plugins.push(
+      new ImageMinimizerPlugin({
+        deleteOriginalAssets: true,
+        filename: "static/media/[name].webp",
+        minimizerOptions: {
+          plugins: ["imagemin-webp"],
+        },
+      })
+    );
+
+    // console.log(config);
+
     return config;
-  }
-  ,
-  exportPathMap: async function() {
+  },
+  pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
+  exportPathMap: async function () {
     const routes = {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/resume': { page: '/resume' },
-      '/skills': { page: '/skills' },
-      '/tools': { page: '/tools' },
-      '/sitemap': { page: '/sitemap' },
-    }
-    return routes
-  }
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/resume": { page: "/resume" },
+      "/skills": { page: "/skills" },
+      "/tools": { page: "/tools" },
+      "/sitemap": { page: "/sitemap" },
+    };
+    return routes;
+  },
 });
