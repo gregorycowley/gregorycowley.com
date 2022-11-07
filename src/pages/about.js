f88045dee31bc476db1a1ae158022405f3b3cd05
Gregory Cowley <gcowley@exploratorium.edu>
2022-11-06 19:42:25 -0800

    Half-fixed older 2021 deployment

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,7 @@
-import matter from "gray-matter";
+import matter from 'gray-matter'
 import React from "react";
 import ReactMarkdown from "react-markdown";
-
-import { LayoutAlt } from "components";
-import { TextContent, Photo} from "components";
-
-import config from "common/configs/config";
+import { Layout, TextContent, Photo } from "../components";
 
 class About extends React.Component {
 	constructor(props) {
@@ -16,32 +12,29 @@ class About extends React.Component {
 
 	render() {
 		return (
-			<LayoutAlt {...this.props}>
+			<Layout {...this.props}>
 				<TextContent className="about-page">
-					<Photo img={this.frontmatter.hero_image} style="about-photo"/>
+					<Photo img={this.frontmatter.hero_image} style="about-photo" />
 					<h1>{this.frontmatter.title}</h1>
 					<h2>{this.frontmatter.subtitle}</h2>
 					<p>{this.frontmatter.description}</p>
 					<p><i>{this.frontmatter.author}</i></p>
 					<ReactMarkdown source={this.markdownBody} />
 				</TextContent>
-			</LayoutAlt>
-		);
+			</Layout>
+		)
 	}
 }
 
 export default About;
 
-About.getInitialProps = async function() {
-	const content = await import("../content/about.md");
+About.getInitialProps = async function () {
+	const config = await import(`../data/config.json`)
+	const content = await import(`../content/about.md`);
 	const data = matter(content.default);
 	return {
 		...config,
 		frontmatter: data.data,
 		content: data.content
-	};
-};
-
-
-
-
+	}
+}
